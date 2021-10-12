4f05e64629aaffd4d0f3347ab9765501dff8d327
unknown <ajaysindolia98@gmail.com>
2021-10-12 18:01:20 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const path=require('path')
 const express=require('express')
 const hbs=require('hbs')
 const app=express()
-
+const port=process.env.PORT || 3000
 //define paths for express config
 const directorypath=path.join(__dirname,'../public')
 const viewspath=path.join(__dirname,'../templates/views')
@@ -91,6 +91,6 @@ app.get('*',(req,res)=>{
    })
 })
 
-app.listen(3000,()=>{
-    console.log("server is started")
+app.listen(port,()=>{
+    console.log("server is started"+port)
 })
